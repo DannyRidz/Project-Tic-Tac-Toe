8d9095fb8b6cea02e0b14eb640582bdedf9d5b3d
DannyRidz <danny.ridz93@gmail.com>
2026-06-16 13:13:51 +0800

    Add initial HTML, CSS, and JavaScript files for Tic Tac Toe project

---
+++ script.js
@@ -0,0 +1,15 @@
+
+/* CREATE a private array with 9 empty spaces
+
+- Function getBoard():
+    - Return the current state of the array
+
+- Function placeMarker(index, marker):
+    - IF the space at [index] is empty:
+        - Put the player's marker into that array index
+        - Return TRUE
+    ELSE:
+        - Return FALSE
+
+*/
+
